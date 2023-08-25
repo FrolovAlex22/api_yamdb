ecc7b7ada9eff12a4fcc4a3accda3489fb53b85e
Александр Фролов <frolov.bsk@yandex.ru>
2023-08-25 16:57:01 +0500

    add urls for "reviews", "api".

--- api_yamdb/api_yamdb/urls.py
+++ api_yamdb/api_yamdb/urls.py
@@ -14,11 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('reviews/', include('reviews.urls', namespace='reviews')),
+    path('api/', include('api.urls', namespace='api')),
     path(
         'redoc/',
         TemplateView.as_view(template_name='redoc.html'),
