9ca331676af04357540227e4358fd85644093358
Александр Фролов <frolov.bsk@yandex.ru>
2023-09-11 10:17:55 +0500

    fix README

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -11,50 +11,6 @@ from rest_framework.serializers import (CurrentUserDefault,
 from reviews.models import Category, Genre, Title, Comment, Review
 
 
-class ReviewSerializer(ModelSerializer):
-    """Сериалайзер модели Review."""
-    author = SlugRelatedField(
-        slug_field='username',
-        read_only=True,
-        default=CurrentUserDefault()
-    )
-    title = SlugRelatedField(
-        slug_field='name',
-        read_only=True
-    )
-
-    class Meta:
-        model = Review
-        fields = '__all__'
-
-    def validate(self, data: OrderedDict) -> OrderedDict:
-        """Защита от повторов отзыва от пользователя."""
-        if self.context['request'].method != 'POST':
-            return data
-        author = self.context['request'].user
-        title_id = self.context['view'].kwargs.get('title_id')
-        if Review.objects.filter(author=author, title_id=title_id).exists():
-            raise ValidationError('Отзыв уже был ранее')
-        return data
-
-
-class CommentSerializer(ModelSerializer):
-    """Сериалайзер модели Comment."""
-    author = SlugRelatedField(
-        slug_field='username',
-        read_only=True,
-        default=CurrentUserDefault()
-    )
-    review = SlugRelatedField(
-        slug_field='text',
-        read_only=True
-    )
-
-    class Meta:
-        model = Comment
-        fields = '__all__'
-
-
 class CategorySerializer(serializers.ModelSerializer):
     """Сериализатор для CategoryViewSet"""
 
@@ -149,3 +105,47 @@ class TitleSerializer(serializers.ModelSerializer):
                 'Год выпуска произведения дожен быть раньше этого года'
             )
         return value
+
+
+class ReviewSerializer(ModelSerializer):
+    """Сериалайзер модели Review."""
+    author = SlugRelatedField(
+        slug_field='username',
+        read_only=True,
+        default=CurrentUserDefault()
+    )
+    title = SlugRelatedField(
+        slug_field='name',
+        read_only=True
+    )
+
+    class Meta:
+        model = Review
+        fields = '__all__'
+
+    def validate(self, data: OrderedDict) -> OrderedDict:
+        """Защита от повторов отзыва от пользователя."""
+        if self.context['request'].method != 'POST':
+            return data
+        author = self.context['request'].user
+        title_id = self.context['view'].kwargs.get('title_id')
+        if Review.objects.filter(author=author, title_id=title_id).exists():
+            raise ValidationError('Отзыв уже был ранее')
+        return data
+
+
+class CommentSerializer(ModelSerializer):
+    """Сериалайзер модели Comment."""
+    author = SlugRelatedField(
+        slug_field='username',
+        read_only=True,
+        default=CurrentUserDefault()
+    )
+    review = SlugRelatedField(
+        slug_field='text',
+        read_only=True
+    )
+
+    class Meta:
+        model = Comment
+        fields = '__all__'
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -29,69 +29,6 @@ from .permissions import (
 )
 
 
-class ReviewViewSet(ModelViewSet):
-    """
-    Отзывы.
-
-    Получение списка всех отзывов: Доступно без токена
-        GET: /titles/{title_id}/reviews/
-    Добавление нового отзыва: Аутентифицированные пользователи
-        POST: /titles/{title_id}/reviews/
-    Получение отзыва по id: Доступно без токена
-        GET: /titles/{title_id}/reviews/{review_id}/
-    Частичное обновление отзыва по id: Автор отзыва, модератор или админ
-        PATCH: /titles/{title_id}/reviews/{review_id}/
-    Удаление отзыва по id: Автор отзыва, модератор или админ
-        DELETE: /titles/{title_id}/reviews/{review_id}/
-    """
-    serializer_class = ReviewSerializer
-    permission_classes = (IsAuthorModeratorAdminOrReadOnly,)
-
-    def get_queryset(self) -> QuerySet:
-        """Возвращает отзывы."""
-        title_id = self.kwargs.get('title_id')
-        title = get_object_or_404(Title, id=title_id)
-        return title.reviews.all()
-
-    def perform_create(self, serializer: ModelSerializer) -> None:
-        """Создаёт отзыв в БД."""
-        title_id = self.kwargs.get('title_id')
-        title = get_object_or_404(Title, pk=title_id)
-        serializer.save(author=self.request.user, title=title)
-
-
-class CommentViewSet(ModelViewSet):
-    """
-    Комментарии к отзывам
-
-    Получение списка всех комментариев к отзыву: Доступно без токена
-        GET: /titles/{title_id}/reviews/{review_id}/comments/
-    Добавление комментария к отзыву: Аутентифицированные пользователи
-        POST: /titles/{title_id}/reviews/{review_id}/comments/
-    Получение комментария к отзыву: Доступно без токена
-        GET: /titles/{title_id}/reviews/{review_id}/comments/{comment_id}/
-    Частичное обновление комментария к отзыву:
-    Автор комментария, модератор или админ
-        PATCH: /titles/{title_id}/reviews/{review_id}/comments/{comment_id}/
-    Удаление комментария к отзыву: Автор комментария, модератор или админ
-        DELETE: /titles/{title_id}/reviews/{review_id}/comments/{comment_id}/
-    """
-    serializer_class = CommentSerializer
-    permission_classes = (IsAuthorModeratorAdminOrReadOnly,)
-
-    def get_queryset(self) -> QuerySet:
-        """Возвращает комментарий."""
-        review_id = self.kwargs.get('review_id')
-        review = get_object_or_404(Review, id=review_id)
-        return review.comments.all()
-
-    def perform_create(self, serializer: ModelSerializer) -> None:
-        """Создаёт комментарий в БД."""
-        review_id = self.kwargs.get('review_id')
-        review = get_object_or_404(Review, pk=review_id)
-        serializer.save(author=self.request.user, review=review)
-
-
 class TitleViewSet(viewsets.ModelViewSet):
     """
     Произведения, к которым пишут отзывы
@@ -183,3 +120,66 @@ class CategoryViewSet(ListCreateDeleteViewSet):
         if self.request.method in ['POST', 'DELETE', 'PUT', 'PATCH']:
             return (IsAdminOrSuperuser(),)
         return super().get_permissions()
+
+
+class ReviewViewSet(ModelViewSet):
+    """
+    Отзывы.
+
+    Получение списка всех отзывов: Доступно без токена
+        GET: /titles/{title_id}/reviews/
+    Добавление нового отзыва: Аутентифицированные пользователи
+        POST: /titles/{title_id}/reviews/
+    Получение отзыва по id: Доступно без токена
+        GET: /titles/{title_id}/reviews/{review_id}/
+    Частичное обновление отзыва по id: Автор отзыва, модератор или админ
+        PATCH: /titles/{title_id}/reviews/{review_id}/
+    Удаление отзыва по id: Автор отзыва, модератор или админ
+        DELETE: /titles/{title_id}/reviews/{review_id}/
+    """
+    serializer_class = ReviewSerializer
+    permission_classes = (IsAuthorModeratorAdminOrReadOnly,)
+
+    def get_queryset(self) -> QuerySet:
+        """Возвращает отзывы."""
+        title_id = self.kwargs.get('title_id')
+        title = get_object_or_404(Title, id=title_id)
+        return title.reviews.all()
+
+    def perform_create(self, serializer: ModelSerializer) -> None:
+        """Создаёт отзыв в БД."""
+        title_id = self.kwargs.get('title_id')
+        title = get_object_or_404(Title, pk=title_id)
+        serializer.save(author=self.request.user, title=title)
+
+
+class CommentViewSet(ModelViewSet):
+    """
+    Комментарии к отзывам
+
+    Получение списка всех комментариев к отзыву: Доступно без токена
+        GET: /titles/{title_id}/reviews/{review_id}/comments/
+    Добавление комментария к отзыву: Аутентифицированные пользователи
+        POST: /titles/{title_id}/reviews/{review_id}/comments/
+    Получение комментария к отзыву: Доступно без токена
+        GET: /titles/{title_id}/reviews/{review_id}/comments/{comment_id}/
+    Частичное обновление комментария к отзыву:
+    Автор комментария, модератор или админ
+        PATCH: /titles/{title_id}/reviews/{review_id}/comments/{comment_id}/
+    Удаление комментария к отзыву: Автор комментария, модератор или админ
+        DELETE: /titles/{title_id}/reviews/{review_id}/comments/{comment_id}/
+    """
+    serializer_class = CommentSerializer
+    permission_classes = (IsAuthorModeratorAdminOrReadOnly,)
+
+    def get_queryset(self) -> QuerySet:
+        """Возвращает комментарий."""
+        review_id = self.kwargs.get('review_id')
+        review = get_object_or_404(Review, id=review_id)
+        return review.comments.all()
+
+    def perform_create(self, serializer: ModelSerializer) -> None:
+        """Создаёт комментарий в БД."""
+        review_id = self.kwargs.get('review_id')
+        review = get_object_or_404(Review, pk=review_id)
+        serializer.save(author=self.request.user, review=review)
